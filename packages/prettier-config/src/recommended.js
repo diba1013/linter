7715c7200103093cbebe7f5a0eefcc0757673d0f
diba1013 <diba1013@web.de>
2023-12-28 13:34:23 +0100

    feat!: change prettier to esm
    Signed-off-by: diba1013 <diba1013@web.de>

---
+++ packages/prettier-config/src/recommended.js
@@ -0,0 +1,42 @@
+/**
+ * Defines the custom configuration options for eslint config options.
+ *
+ * @typedef {Object} CustomLinterOptions
+ */
+
+/**
+ *
+ * @param {CustomLinterOptions} options
+ * @returns {import('prettier').Config}
+ */
+export function defineConfig() {
+	return {
+		editorconfig: true,
+		pluginSearchDirs: false,
+
+		printWidth: 120,
+		tabWidth: 4,
+		useTabs: true,
+		trailingComma: "all",
+		singleAttributePerLine: true,
+
+		overrides: [
+			{
+				files: ["*.yaml", "*.yml"],
+				options: {
+					useTabs: false,
+					tabWidth: 2,
+				},
+			},
+			{
+				files: ["*.html", "*.vue", "*.css", "*.scss"],
+				plugins: ["prettier-plugin-tailwindcss"],
+			},
+		],
+	};
+}
+
+/**
+ * @type {import('prettier').Config}
+ */
+export default defineConfig();
